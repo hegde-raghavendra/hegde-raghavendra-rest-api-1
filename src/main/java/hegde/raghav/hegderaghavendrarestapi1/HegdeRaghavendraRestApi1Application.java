8b8696f6ef89ffdbacbd235d3ba64e25723f5999
Raghavendra Hegde <raghavendra.g.hegde@gmail.com>
2022-03-05 12:44:03 -0600

    first commit

---
+++ src/main/java/hegde/raghav/hegderaghavendrarestapi1/HegdeRaghavendraRestApi1Application.java
@@ -0,0 +1,13 @@
+package hegde.raghav.hegderaghavendrarestapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HegdeRaghavendraRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HegdeRaghavendraRestApi1Application.class, args);
+	}
+
+}
